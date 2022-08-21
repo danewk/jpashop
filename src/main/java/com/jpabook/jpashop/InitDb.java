c01d43b4eea31609480883ae430dcb391a1c2a90
kdanwoo <kdanwoo@jobis.co>
2022-08-21 15:40:20 +0900

    DB 초기화 작업

---
+++ src/main/java/com/jpabook/jpashop/InitDb.java
@@ -0,0 +1,19 @@
+package com.jpabook.jpashop;
+
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+  private final InitService initService;
+
+  @PostConstruct
+  public void init() {
+    initService.dbInit1();
+    initService.dbInit2();
+  }
+
+}
